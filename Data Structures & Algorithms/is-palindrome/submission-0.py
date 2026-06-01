67cd14f817358eb1932e705dd9702e9986b3dfc9
Malachai Onwona <130776588+MalachaiOnwona@users.noreply.github.com>
2026-05-31 22:37:29 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,15 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        result = ""
+
+        for char in s:
+            if char.isalnum():
+                result += char.lower()
+        
+        reverse = result[::-1]
+
+        return result == reverse
+        
