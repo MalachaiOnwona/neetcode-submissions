a31fc8285c72fe910f241d79fe649993be9d0722
Malachai Onwona <130776588+MalachaiOnwona@users.noreply.github.com>
2026-07-01 07:37:47 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,36 @@
+class MinStack:
+
+    def __init__(self):
+
+        self.stack = []
+        self.min_stack = [] #Stack to keep Track of Min values
+        
+
+    def push(self, val: int) -> None:
+
+        self.stack.append(val)
+
+        if self.min_stack:
+
+            val = min(val, self.min_stack[-1])
+        
+        self.min_stack.append(val)
+
+
+        
+
+    def pop(self) -> None:
+
+        self.stack.pop()
+        self.min_stack.pop()
+        
+
+    def top(self) -> int:
+
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+
+        return self.min_stack[-1]
+        
